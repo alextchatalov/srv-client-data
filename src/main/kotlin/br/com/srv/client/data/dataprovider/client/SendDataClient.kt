e3f94b6ad7a22a6ed504b20c920d599bf6a2a3ae
Alex <alex_tr1992@hotmail.com>
2022-05-19 15:47:34 -0300

    correções

--- src/main/kotlin/br/com/srv/client/data/dataprovider/client/SendDataClient.kt
+++ src/main/kotlin/br/com/srv/client/data/dataprovider/client/SendDataClient.kt
@@ -1,12 +1,11 @@
 package br.com.srv.client.data.dataprovider.client
 
-import br.com.srv.client.data.config.FeignConfiguration
 import br.com.srv.client.data.core.model.SendEquipment
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@FeignClient(url = "https://srv-receiver-data.azurewebsites.net", name = "send-data", configuration = [FeignConfiguration::class])
+@FeignClient(url = "https://srv-receiver-data.azurewebsites.net", name = "send-data")
 interface SendDataClient {
 
     @PostMapping("/receiver")
